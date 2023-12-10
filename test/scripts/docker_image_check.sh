3cb9afd4c075d6546987c4001c7645532c203cf1
rakeshpb90 <pb.rakesh90@gmail.com>
2023-12-10 10:50:36 +0530

    Test | test

--- test/scripts/docker_image_check.sh
+++ test/scripts/docker_image_check.sh
@@ -4,11 +4,12 @@
 APP_NAME=$1
 IMAGE_NAME=$2
 REGION=$3
+
 # Display the values
 echo "APP_NAME: ${APP_NAME}"
 echo "IMAGE_NAME: ${IMAGE_NAME}"
 
-# Checking if Docker image exists in the registry
+# Check if the Docker image exists in the registry
 IMAGE_EXIST=$(aws ecr describe-images --repository-name "${APP_NAME}" --image-ids imageTag="${IMAGE_NAME}" --region ${REGION})
 
 # Check if the image exists
@@ -16,4 +17,5 @@ if [ -n "$IMAGE_EXIST" ]; then
   echo "Image exists"
 else
   echo "Image does not exist"
+  exit 1
 fi
