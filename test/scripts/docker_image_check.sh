2118e7d4e0b9e0b78d002493a9126546e7d09555
rakeshpb90 <pb.rakesh90@gmail.com>
2024-04-02 16:47:27 +0530

    test

--- test/scripts/docker_image_check.sh
+++ test/scripts/docker_image_check.sh
@@ -13,39 +13,44 @@ REGION=$3
 ENV_NAME=$4
 
 # Display the values
+echo "INFO: Setting environment variables..."
 echo "INFO: APP: ${APP}"
 echo "INFO: IMAGE_NAME: ${IMAGE_NAME}"
 echo "INFO: REGION: ${REGION}"
 echo "INFO: ENV_NAME: ${ENV_NAME}"
 
 # Define the path to the values file
-VALUES_FILE="${CODEBUILD_SRC_DIR}/${ENV_NAME}/${APP_NAME}/values.yaml"
+VALUES_FILE="${CODEBUILD_SRC_DIR}/${ENV_NAME}/${APP}/values.yaml"
 
 # Check if the values file exists
+echo "INFO: Checking if values file exists..."
 if [ ! -f "$VALUES_FILE" ]; then
   echo "ERROR: Values file not found: ${VALUES_FILE}"
   exit 1
 fi
 
 # Extract the image name from the file
-IMAGE_NAME=$(awk '/tag:/ {print $2}' "${VALUES_FILE}")
+echo "INFO: Extracting image name from the values file..."
+IMAGE_OLD_IMAGE=$(awk '/tag:/ {print $2}' "${VALUES_FILE}")
 
 # Check if the image name is found
-if [ -z "$IMAGE_NAME" ]; then
+echo "INFO: Checking if image name is found in the file..."
+if [ -z "$IMAGE_OLD_IMAGE" ]; then
   echo "ERROR: Image name not found in the file: ${VALUES_FILE}"
   exit 1
 fi
 
-echo "INFO: Docker Image Name: ${APP}/${IMAGE_NAME}"
+echo "INFO: Docker Image in the values file: ${APP}/${IMAGE_OLD_IMAGE}"
 
-# Check if the Docker image exists in the registry
+# Check if the user-supplied Docker image exists in the registry
+echo "INFO: Checking if the user-supplied Docker image exists in the registry..."
 IMAGE_EXIST=$(aws ecr describe-images --repository-name "${APP}" --image-ids imageTag="${IMAGE_NAME}" --region "${REGION}")
 
-# Check if the image exists
-if [ -z "$IMAGE_EXIST" ]; then
+echo ${IMAGE_EXIST}
+# Validate the response
+if [ -z "$IMAGE_EXIST" ] || [ "$(echo "$IMAGE_EXIST" | jq '.imageDetails')" = "null" ]; then
   echo "ERROR: Image ${APP}/${IMAGE_NAME} does not exist in ${REGION} ECR repository"
   exit 1
 else
   echo "INFO: Image ${APP}/${IMAGE_NAME} exists in ${REGION} ECR repository"
 fi
-
