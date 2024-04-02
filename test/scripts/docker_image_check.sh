5b655a283b83c3ffab3ce8a30c3b3687fe1d4d34
rakeshpb90 <pb.rakesh90@gmail.com>
2024-04-02 16:55:53 +0530

    test

--- test/scripts/docker_image_check.sh
+++ test/scripts/docker_image_check.sh
@@ -6,7 +6,6 @@ if [ "$#" -ne 4 ]; then
   exit 1
 fi
 
-# Set the environment variables
 APP=$1
 IMAGE_NAME=$2
 REGION=$3
@@ -22,18 +21,18 @@ echo "INFO: ENV_NAME: ${ENV_NAME}"
 # Define the path to the values file
 VALUES_FILE="${CODEBUILD_SRC_DIR}/${ENV_NAME}/${APP}/values.yaml"
 
-# Check if the values file exists
+
 echo -e "\nINFO: Checking if values file exists..."
 if [ ! -f "$VALUES_FILE" ]; then
   echo "ERROR: Values file not found: ${VALUES_FILE}"
   exit 1
 fi
 
-# Extract the image name from the file
+
 echo -e "\nINFO: Extracting image name from the values file..."
 IMAGE_OLD_IMAGE=$(awk '/tag:/ {print $2}' "${VALUES_FILE}")
 
-# Check if the image name is found
+
 echo -e "\nINFO: Checking if image name is found in the file..."
 if [ -z "$IMAGE_OLD_IMAGE" ]; then
   echo "ERROR: Image name not found in the file: ${VALUES_FILE}"
@@ -42,11 +41,11 @@ fi
 
 echo "INFO: Docker Image in the values file: ${APP}/${IMAGE_OLD_IMAGE}"
 
-# Check if the user-supplied Docker image exists in the registry
+
 echo -e "\nINFO: Checking if the user-supplied Docker image exists in the registry..."
 IMAGE_EXIST=$(aws ecr describe-images --repository-name "${APP}" --image-ids imageTag="${IMAGE_NAME}" --region "${REGION}")
 
-# Validate the response
+
 if [ -z "$IMAGE_EXIST" ] || [ "$(echo "$IMAGE_EXIST" | jq '.imageDetails')" = "null" ]; then
   echo "ERROR: Image ${APP}/${IMAGE_NAME} does not exist in ${REGION} ECR repository"
   exit 1
