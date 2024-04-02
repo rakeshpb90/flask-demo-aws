383c19e86654e2fa947a024aff2701a5191a6453
rakeshpb90 <pb.rakesh90@gmail.com>
2024-04-02 17:56:31 +0530

    test

--- test/scripts/update_values.sh
+++ test/scripts/update_values.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 ENV_NAME APP IMAGE_NAME"
+  exit 1
+fi
+
 # Set environment variables
 ENV_NAME=$1
-APP_NAME=$2
+APP=$2
 IMAGE_NAME=$3
-VALUES_FILE="${CODEBUILD_SRC_DIR}/${ENV_NAME}/${APP_NAME}/values.yaml"
+VALUES_FILE="${CODEBUILD_SRC_DIR}/${ENV_NAME}/${APP}/values.yaml"
 
-# Execute the first yq command to retrieve the current image tag
+# Enable automatic exit on error
+set -e
+
+
+echo -e "\nINFO: Setting environment variables..."
+echo "INFO: ENV_NAME: ${ENV_NAME}"
+echo "INFO: APP: ${APP}"
+echo "INFO: IMAGE_NAME: ${IMAGE_NAME}"
+
+
+echo -e "\nINFO: Executing the first yq command to retrieve the current image tag..."
 current_image_tag=$(yq e '.image.tag' "${VALUES_FILE}")
 
-# Check if the first yq command succeeded
-if [ $? -ne 0 ]; then
-    echo "ERROR: Unable to retrieve the current image tag from ${VALUES_FILE}. Exiting."
-    exit 1
-fi
 
-# Display the result of the first yq command
 echo "INFO: Current image tag: ${current_image_tag}"
 
-# Execute the second yq command to update the image tag in values file
+
+echo -e "\nINFO: Executing the second yq command to update the image tag in the values file..."
 yq e ".image.tag = \"${IMAGE_NAME}\"" -i "${VALUES_FILE}"
 
-# Check if the yq update command succeeded
-if [ $? -ne 0 ]; then
-    echo "ERROR: Unable to update the image tag in ${VALUES_FILE}. Exiting."
-    exit 1
-fi
 
-# Display lines containing 'tag' in ${VALUES_FILE} after the update
-echo "INFO: Lines containing 'tag' in ${VALUES_FILE} after update:"
-grep -w "tag" ${VALUES_FILE}
+echo -e "\nINFO: Lines containing 'tag' in ${VALUES_FILE} after update:"
+grep -w "tag" "${VALUES_FILE}"
 
-# Display a message indicating the update
-echo "INFO: Updated image tag to: ${IMAGE_NAME}"
+echo -e "\nINFO: Updated image tag to: ${IMAGE_NAME}"
